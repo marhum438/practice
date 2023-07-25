a431205bc456ce56de320eb8984fbcdbdc98246e
marhum438 <138965889+marhum438@users.noreply.github.com>
2023-07-25 14:47:59 +0500

    .......

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Container.dart';
-import 'package:flutter_application_1/alinment.dart';
-import 'package:flutter_application_1/sinin.dart';
 
-import 'increment.dart';
-
-void main() {
+import 'material.dart';
+ void main(List<String> args) {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-     // home: Sinin(),
-    // home:increment(),
-   // home: Types_Container(),
-   home: Types_Alinment(),
-    );
-  }
-}
-/*
-
-data 1=>2 : Sinin()
-increment : increment(),
-container types : Types_Container(),
+};
 
 
 
 
-*/
