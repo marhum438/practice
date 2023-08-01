06d63855acea5f01c1137333aab111760f9ed57a
kashaf172725 <134833348+kashaf172725@users.noreply.github.com>
2023-07-31 22:48:33 -0700

    ............

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:flutter_application_1/main.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/test.dart';
 import 'package:flutter_application_1/utils/const.dart';
 
-import 'k_screens/hiddendrawer_screens/hiden_drawer.dart';
+
 
 void main(){
   runApp(MyApp());
